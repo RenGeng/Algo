f6dfb05cac0cc8c9e1a714c552c9138106e7aef8
RenGeng <geng2608@hotmail.com>
2017-04-27 00:09:05 +0200

    graphe 1 marche et pas 2

--- graphe.c
+++ graphe.c
@@ -1,19 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define TAILLE_MAX 6
-// Définition à la main du graphe
-int graphe[TAILLE_MAX][TAILLE_MAX] = {
 
-						{0,0,0,1,1,0},
-						{0,0,0,1,0,1},
-						{1,1,0,0,1,0},
-						{1,1,1,0,1,0},
-						{0,1,1,0,0,1},
-						{0,1,1,0,1,0},
+// Définition à la main du graphe (voir le document word pour avoir une image)
+
+
+#define TAILLE_MAX 6
+int graphe[TAILLE_MAX][TAILLE_MAX] = { // graphe 1
+
+						{0,1,0,1,0,1},
+						{1,0,1,0,1,0},
+						{0,1,0,0,1,0},
+						{1,0,0,0,1,0},
+						{0,1,1,1,0,1},
+						{1,0,0,0,1,0},
 				   					 
 				   					 };
 
+
+/*
+#define TAILLE_MAX 7
+int graphe[TAILLE_MAX][TAILLE_MAX] = { // graphe 2 marche pas
+
+						{0,1,0,0,0,0,0},
+						{1,0,1,1,0,0,0},
+						{0,1,0,0,0,0,1},
+						{0,1,0,0,1,0,1},
+						{0,0,0,1,0,1,0},
+						{0,0,0,0,1,0,1},
+				   		{0,0,1,1,0,1,0},
+				   					 };
+*/
 
 //fonction qui calcule le nb d'arc (ie. le nb de voisin) d'un noeud
 int calcul_nb_arc(int graphe[][TAILLE_MAX],int numero_noeud)
@@ -55,33 +72,53 @@ int in_tableau(int val,int tab[]) // Parcourir un tableau sans savoir sa taille.
 	return 0;
 }
 
+//fonction pour avoir le noeud qui a le plus de voisin
+int max_voisin(int graphe[][TAILLE_MAX])
+{
+	int i,nb_max = -1,nb_voisin,noeud;
+	for(i=0;i<TAILLE_MAX;i++)
+	{
+		nb_voisin = calcul_nb_arc(graphe,i);
+		if(nb_voisin>nb_max)
+		{
+			nb_max = nb_voisin;
+			noeud = i;
+		}
+	}
+	return noeud;
+}
+
 void main(void)
 {
 
-	int i,j,k=1;
+	int i,j,k=1,nb_voisin,cond_dedans,noeud_max_voisin; //cond_dedans vaut 1 si aucun élément de liste_voisin est dans dominant_set, 0 sinon
+	int *liste_voisin;
 	int dominant_set[TAILLE_MAX-1];
 	for (i = 0; i < TAILLE_MAX; i++) // pour initialiser le tableau à -1 et avoir un point avec un nb de voisin élévé
 	{
 		dominant_set[i] = -1;
-		if(calcul_nb_arc(graphe,i) >= 3)
-			{
-				dominant_set[0] = i;
-
-			}
 	}
+	noeud_max_voisin = max_voisin(graphe);
+	dominant_set[0] = noeud_max_voisin;
+
+	printf("INIT\n");
+	for (int i = 0; i < TAILLE_MAX; ++i)
+	{
+		printf("%d\n",dominant_set[i]);
+	}
+
 
 	for(i=0;i<TAILLE_MAX;i++)
 	{
-		if(calcul_nb_arc(graphe,i) >= 3)
+		liste_voisin = voisin(graphe,i);
+		nb_voisin = calcul_nb_arc(graphe,i);
+		if (i==dominant_set[0]) continue;
+		for(j=0;j<nb_voisin;j++)
 		{
-			for(j=0;j<TAILLE_MAX;j++) //boucle pour regarder si le noeud qu'on considère est oui ou non un voisin des noeuds qu'on a déjà ajouté
-			{
-				if (!in_tableau(i,voisin(graphe,dominant_set[j])))
-				{
-					dominant_set[k++] = i;
-				}
-			}
+			if(!in_tableau(liste_voisin[j],dominant_set)) cond_dedans = 1;
+			else cond_dedans = 0;
 		}
+		if(cond_dedans) dominant_set[k++] = i;
 	}
 
 
@@ -96,10 +133,20 @@ void main(void)
 	printf("\n\n\n\n\n");
 	int *b = voisin(graphe,0); 
 	*/
+	printf("AFTER\n");
 	for (int i = 0; i < TAILLE_MAX; ++i)
 	{
 		printf("%d\n",dominant_set[i]);
 	}
+/*
+	int *liste = voisin(graphe,1);
 
-	
+	printf("VOISIN de 4\n");
+	printf("nb de voisin =%d\n",calcul_nb_arc(graphe,1));
+	for (int i = 0; i < calcul_nb_arc(graphe,1); ++i)
+	{
+		printf("%d\n",liste[i]);
+	}
+
+*/	
 }
